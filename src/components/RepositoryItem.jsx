57f3c707304bfe995344c27eb0e4a2b463fedbbd
Ittalo Steltter <ittalolz@gmail.com>
2022-03-29 15:24:24 -0400

    feat: fetch github api

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>Acessar respositório</a>
+      <a href={props.repository.html_url}>Acessar respositório</a>
     </li>
   );
 }
